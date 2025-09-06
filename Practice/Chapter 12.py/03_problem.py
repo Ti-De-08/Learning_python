b72b2b5bd598810942ff976bf8d967d85b7fa1b5
Ti-De-08 <d.tirtha@op.iitg.ac.in>
2025-09-06 23:55:03 +0600

    Delete Practice/Chapter 12.py directory

--- Practice/Chapter 12.py/03_problem.py
+++
@@ -1,9 +0,0 @@
-""" Write a LIST COMPREHENSION to print a list which contains the multiplication table of a
-user entered number."""
-
-
-n = int(input("Enter a number: "))
-
-table = [n*i for i in range(1, 11)]
-print(f"The multiplication table of {n} is:")
-print(table)
--- Practice/Chapter 12.py/04_problem.py
+++
@@ -1,7 +0,0 @@
-
-try:
-    a = int(input("Enter a Number: "))
-    b = int(input("Enter another Number: "))
-    print(a/b)
-except ZeroDivisionError as z:
-    print("Infinite:", z)
